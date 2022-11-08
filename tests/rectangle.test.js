e146eeeae498cb15725d3ac80b8accaa61f32d0f
Flatro <pawel01019@gmail.com>
2022-11-08 17:53:17 +0100

    Final commit

--- tests/rectangle.test.js
+++ tests/rectangle.test.js
@@ -9,3 +9,13 @@ test('should properly return rectangle perimeter', () => {
     const output = getRectanglePerimeter(2, 4);
     expect(output).toBe(16);
 });
+
+test('should properly return rectangle info', () => {
+    const output = getRectangleInfo(2, 4);
+    expect(output).toBe(console.log(`The perimeter of a rectangle is 16 and the area is 8`));
+});
+
+test('should properly return rectangle info', () => {
+    const output = getRectangleInfo(16, 4);
+    expect(output).toBe(undefined);
+});
